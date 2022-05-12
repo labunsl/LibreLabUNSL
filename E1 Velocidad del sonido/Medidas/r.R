30b1226ca52cdd8b8171820a76c539a2b607b642
Kiernan Preve <44746791+KierPrev@users.noreply.github.com>
2022-05-12 13:07:04 -0300

    obtención de delta t para calibración

---
+++ E1 Velocidad del sonido/Medidas/r.R
@@ -0,0 +1,27 @@
+library(DataEditR)
+
+datos <- read.csv("Documents/GitHub/LibreLabUNSL/E1 Velocidad del sonido/Medidas/data.csv")
+
+plot(datos$Distancia , datos$TOF)
+
+datosClean <- data_edit(datos)
+
+#### PLOTS ####
+# Distancia vs Vel Sonido, bruto
+plot(datos$Distancia, datos$Distancia/datos$TOF)
+# Distancia vs Vel Sonido
+plot(datosClean$Distancia, datosClean$Distancia/datosClean$TOF)
+mean(datosClean$Distancia/datosClean$TOF)
+# Distancia vs TOF
+plot(datosClean$Distancia, datosClean$TOF)
+
+
+#### AJUSTE LINEAR ####
+velSonidoRef=344.72
+ajuste <- lm(datosClean$TOF - 1/velSonidoRef*datosClean$Distancia ~ 1)
+ajuste$coefficients
+ordenada <- ajuste$coefficients[[1]]
+
+datosClean$TOF.corr <- datosClean$TOF-ajuste$coefficients[[1]]
+plot(datosClean$Distancia, datosClean$Distancia/datosClean$TOF.corr)
+mean(datosClean$Distancia/datosClean$TOF.corr)
