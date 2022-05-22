6bb1788d4954881ea10433a4ff6ef4b4c7e433d3
Kiernan Preve <44746791+KierPrev@users.noreply.github.com>
2022-05-21 21:39:55 -0300

    añadir medición de velocidad y aceleración, fix frec muestreo

--- E1 Velocidad del sonido/MedirVelocidad/MedirVelocidad.ino
+++ E1 Velocidad del sonido/MedirVelocidad/MedirVelocidad.ino
@@ -2,20 +2,20 @@
 #include "QList.h"
 #include <Adafruit_AHT10.h>
 
-elapsedMillis timeElapsed;
-elapsedMillis tiempoTemp;
+elapsedMillis timeElapsed, tiempoTemp;
+unsigned long tMedida, tAnt;
 Adafruit_AHT10 aht;
 
 
 
 int i;
 int medida;
-float frecMuestreo = 1;   // Esto va a ser variable en la INTERFAZ  ========= La máxima es 35 para nMedidas=5
+float frecMuestreo = 15;   // Esto va a ser variable en la INTERFAZ  ========= La máxima es 30 para nMedidas=5
 const int nMedidas = 5;  
 int listaMedidas[nMedidas] = {};
-float suma = 0.0;
+float suma, v, dAnt, vAnt = 0.0;
 int nOutliers = 1;   //// Esto va a haber que ajustarlo para q sea un % < a mayor nMedidas
-float d;
+float d, a;
 int primerTemp = 1;
 float velSon;
 sensors_event_t humidity, temp;
@@ -52,13 +52,11 @@ void setup(){
 
 void loop() {
 
-  //unsigned long lasttime = millis();   Iniciando timer para ver cuánto tiempo tarda una medida promediada
-  elapsedMillis midiendo = 0;
+  elapsedMillis medicion;
+  medicion=0;
 
-  
-  while (midiendo <= 1000/frecMuestreo-27.8803*1.92) {
-    
-  }
+  //unsigned long lasttime = millis();   Iniciando timer para ver cuánto tiempo tarda una medida promediada
+ 
   
 
      for(int i =1; i <= nMedidas; i++){     // Cuántas mediciones queremos promediar
@@ -77,68 +75,76 @@ void loop() {
       listaMedidas[i] = medida;   // Agregamos medida individual a la lista de medidas
       
       //Serial.println(medida);   // Nos muestra las medidas individuales
+      
       }
-
-//Serial.println(millis() - lasttime);   Mostrando tiempo de medida promediada
-//lasttime=0;
-
-sort(listaMedidas,nMedidas);   // Ordenamos lista 
+      tMedida=(long)timeElapsed;
 
 
 
-QList<int> listaSmart;      // Pasamos lista a Qlist
-for(int i =1; i < nMedidas; i++){
-  listaSmart.push_back(listaMedidas[i]);
+      sort(listaMedidas,nMedidas);   // Ordenamos lista 
+      
+      
+      
+      QList<int> listaSmart;      // Pasamos lista a Qlist
+      for(int i =1; i < nMedidas; i++){
+        listaSmart.push_back(listaMedidas[i]);
+      }
+      
+      
+      
+      
+      for(int i =1; i < nOutliers; i++){   // Sacamos primeros outliers
+        listaSmart.pop_front();
+      }
+       
+      for(int i =1; i < nOutliers; i++){   // Sacamos ultimos outliers
+        listaSmart.pop_back();
+      }
+      
+      /*
+      Serial.println("Ordenadas y sin outliers: ");
+      for(i =1; i <= listaSmart.size(); i++){  
+        Serial.println(listaSmart.at(i));
+      }
+      */
+      
+      // CALCULEMOS MEDIA
+      for(int i =1; i <= listaSmart.size(); i++){  
+        suma += listaSmart.at(i);
+      }
+      float tof = suma/listaSmart.size()/2000000;
+      suma=0;
+      
+      
+      
+      if (tiempoTemp >=60000 || primerTemp == 1) {
+        aht.getEvent(&humidity, &temp);  //Leer sensor Temp y humedad
+        tiempoTemp=0;
+        primerTemp=0;  
+      }
+      
+      
+      
+      velSon=331.67*sqrt((temp.temperature+273.15)/273.15);
+      d=velSon*(tof+0.0000603491);
+      v=(d-dAnt)/((tMedida-tAnt)/1000.0);
+      a=(v-vAnt)/((tMedida-tAnt)/1000.0);
+      
+      Serial.print(timeElapsed/1000.0,7);
+      Serial.print(",");
+      Serial.print(d,5);
+      Serial.print(",");
+      Serial.print(v,7);
+      Serial.print(",");
+      Serial.println(a,7);
+      dAnt=d;
+      vAnt=v;
+      tAnt=(long)tMedida;
+      
+      while (medicion/1000.0   < 1.0/frecMuestreo) {
+          
+          }
+
 }
 
-
-
-
-for(int i =1; i < nOutliers; i++){   // Sacamos primeros outliers
-  listaSmart.pop_front();
-}
- 
-for(int i =1; i < nOutliers; i++){   // Sacamos ultimos outliers
-  listaSmart.pop_back();
-}
-
-/*
-Serial.println("Ordenadas y sin outliers: ");
-for(i =1; i <= listaSmart.size(); i++){  
-  Serial.println(listaSmart.at(i));
-}
-*/
-
-// CALCULEMOS MEDIA
-for(int i =1; i <= listaSmart.size(); i++){  
-  suma += listaSmart.at(i);
-}
-float tof = suma/listaSmart.size()/2000000;
-
-
-if (tiempoTemp >=60000 || primerTemp == 1) {
-  aht.getEvent(&humidity, &temp);  //Leer sensor Temp y humedad
-  tiempoTemp=0;
-  primerTemp=0;  
-}
-
-
-//Serial.print(tof,8);
-//Serial.print(",");
-//Serial.println(temp.temperature);
-//Serial.println(343*tof,5);
-
-velSon=331.67*sqrt((temp.temperature+273.15)/273.15);
-d=velSon*(tof+0.000063);
-Serial.println(timeElapsed/1000.0,5);
-//Serial.print(",");
-//Serial.println(d,10);
-
-
-//Serial.println(humidity.relative_humidity);
-suma=0;
-
-//while(1){};
-
-}
   
